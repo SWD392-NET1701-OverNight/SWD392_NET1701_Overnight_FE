4110579156d6470a0b896bc01ca51828fdce8c8c
DinhNhatNam <dinhnhatnamnoel2002@gmail.com>
2024-06-17 21:57:47 +0700

    merge

--- jelwery-shop/src/feature/auth/authApi.js
+++ jelwery-shop/src/feature/auth/authApi.js
@@ -3,14 +3,11 @@ import axiosClient from '../../api/axiosClient'
 const authAPI = {
   login: (data) => axiosClient.post('/api/Users/login', data),
   signIn: (data) => axiosClient.post('/api/Users/register', data),
-<<<<<<< HEAD
   confirmEmail : (data) => axiosClient.post(`/api/Users/Confirm-email?email=${data}`,data),
   resetPassword : (data) => 
   axiosClient.post(`/api/Users/Reset-passworf?email=${data.email}&password=${data.password}&otp=${data.OTP}`, data),
-=======
   getUerById: (id) => axiosClient.get(`/api/Users/get-user/${id}`),
   updateUser: (data, id) => axiosClient.put(`/api/Users/update-profile/${id}`, data),
->>>>>>> main
 }
 
 export default authAPI
