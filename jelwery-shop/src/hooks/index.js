0fea540d11fa2cb05da278f5a18936cdd8fa5173
nmkhang2632002 <nmkhang2632002@gmail.com>
2024-06-30 21:27:23 +0700

    request 3 and update order

--- jelwery-shop/src/hooks/index.js
+++ jelwery-shop/src/hooks/index.js
@@ -1,2 +1,3 @@
 export * from './useLogout'
 export * from './useCheckout'
+export * from './useUploadImage'
---
+++ jelwery-shop/src/hooks/useUploadImage.js
@@ -0,0 +1,22 @@
+import { useEffect, useRef, useState } from 'react'
+
+export function useUploadImage() {
+  const cloudinaryRef = useRef()
+  const widgetRef = useRef()
+  const [imageUrl, setImageUrl] = useState()
+  useEffect(() => {
+    cloudinaryRef.current = window.cloudinary
+    widgetRef.current = cloudinaryRef.current.createUploadWidget(
+      {
+        cloudName: 'dlrpmjdzz',
+        uploadPreset: 'l5mgpnq4',
+      },
+      function (error, result) {
+        if (result.event === 'success') {
+          setImageUrl(result.info.secure_url)
+        }
+      },
+    )
+  }, [])
+  return { hanldeUpload: () => widgetRef.current.open(), imageUrl }
+}
