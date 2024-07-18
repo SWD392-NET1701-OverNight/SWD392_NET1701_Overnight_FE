54cb8c4071433465e2f116a2bbe7415a37ea0d22
nmkhang2632002 <nmkhang2632002@gmail.com>
2024-07-18 14:11:38 +0700

    fix

--- jelwery-shop/src/schema/createProductSchema.js
+++ jelwery-shop/src/schema/createProductSchema.js
@@ -1,30 +1,23 @@
+import { z } from 'zod'
 export const createProductSchema = z.object({
-  productName: z
-    .string()
-    .min(4, {
-      message: 'Product name must be at least 4 characters long to ensure clarity and uniqueness.',
-    }),
-  description: z
-    .string()
-    .min(4, {
-      message: 'Description must be at least 4 characters long to provide enough detail.',
-    }),
+  productName: z.string().min(4, {
+    message: 'Product name must be at least 4 characters long to ensure clarity and uniqueness.',
+  }),
+  description: z.string().min(4, {
+    message: 'Description must be at least 4 characters long to provide enough detail.',
+  }),
   categoryID: z
     .string()
     .min(1, { message: 'Category ID is required to categorize the product properly.' }),
 })
 
 export const createProductSchemaBySystem = z.object({
-  productName: z
-    .string()
-    .min(4, {
-      message: 'Product name must be at least 4 characters long to ensure clarity and uniqueness.',
-    }),
-  description: z
-    .string()
-    .min(4, {
-      message: 'Description must be at least 4 characters long to provide enough detail.',
-    }),
+  productName: z.string().min(4, {
+    message: 'Product name must be at least 4 characters long to ensure clarity and uniqueness.',
+  }),
+  description: z.string().min(4, {
+    message: 'Description must be at least 4 characters long to provide enough detail.',
+  }),
   categoryID: z
     .string()
     .min(1, { message: 'Category ID is required to categorize the product properly.' }),
